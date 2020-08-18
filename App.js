f44608e0807dfa32ef81eba28cc7397ec11547a7
rony646 <rony646@users.noreply.github.com>
2020-08-18 12:29:17 -0300

    outputting text

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
+
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState("")
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
-          style={styles.input}/>
-        <Button title="ADD"/>
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <View> 
+      <View>
+          {courseGoals.map((goal, index) => <Text key={index}>{goal}</Text>)}
       </View>
 
       <StatusBar style="auto" />
